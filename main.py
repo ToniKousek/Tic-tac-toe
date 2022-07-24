da0a907f4098322d4a68accae26e49144b18f762
DisguisedMamal <toni.kousek@gmail.com>
2022-07-24 14:26:18 +0200

    prettyfied the code

--- main.py
+++ main.py
@@ -8,19 +8,16 @@ from time import sleep
 
 # its spaces for eazy printing the board
 # spaces mean nobody played there
-board=[" "," "," ", " "," "," ", " "," "," "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 # Change this to have diffrent simbol placed on the board
-player_string = ("X","O")
+player_string = ("X", "O")
 
 move_counter = 0
 on_move = player_string[0]
 
 
-
-
-
-#the beggining printing
+# the beggining printing
 def start_of_game_printing():
     sleep(1)
     print()
@@ -34,16 +31,18 @@ def start_of_game_printing():
     print("1|2|3\n-----\n4|5|6\n-----\n7|8|9")
     sleep(3)
     print()
+
+
 start_of_game_printing()
 
 
 # to see clearer / to bo more organized
 # printing the board fuction
 def print_board():
-    for i,e in enumerate(board):
+    for i, e in enumerate(board):
         # if not end line
         if (i + 1) % 3 != 0:
-            	print(f"{e}|", end = "")
+            print(f"{e}|", end="")
         elif i >= 8:
             print(e)
         else:
@@ -54,10 +53,10 @@ def print_board():
 def start_over():
     global move_counter, board, on_move
     move_counter = 0
-    board=[" "," "," ", " "," "," ", " "," "," "]
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     on_move = player_string[0]
 
-    #change this number(it can be a integer or a float) to change how many seconds to freeze the screen
+    # change this number(it can be a integer or a float) to change how many seconds to freeze the screen
     sleep(3.5)
     if name == 'nt':
         system('cls')
@@ -65,41 +64,36 @@ def start_over():
         system('clear')
 
 
-#to see if somebody won
+# to see if somebody won
 def winning_conditions():
-    #three in a row
-    for i in range(0,9,3):
+    # three in a row
+    for i in range(0, 9, 3):
         if board[i] != " ":
             if board[i] == on_move and board[i+1] == on_move and board[i+2] == on_move:
                 win()
-    #three in a collumn
+    # three in a collumn
     for i in range(3):
         if board[i] != " ":
             if board[i] == on_move and board[i+3] == on_move and board[i+6] == on_move:
                 win()
-    #three in a diagonal
-    #from upper left to bottom right
+    # three in a diagonal
+    # from upper left to bottom right
     if board[0] != " ":
         if board[0] == on_move and board[4] == on_move and board[8] == on_move:
             win()
-    #from upper right to bottom left
+    # from upper right to bottom left
     if board[2] != " ":
         if board[2] == on_move and board[4] == on_move and board[6] == on_move:
             win()
-    
 
-#to be organized
+
+# to be organized
 def win():
     print()
     print(f"{on_move}'s won!")
     sleep(1)
     print("Congratulations!")
     start_over()
-
-
-
-
-
 
 
 # main loop
@@ -116,7 +110,7 @@ while True:
         start_over()
         break
     print()
-    
+
     if move < 1:
         print("Shutting down now!")
         start_over()
@@ -147,7 +141,7 @@ while True:
         on_move = player_string[1]
     else:
         on_move = player_string[0]
-    
+
     # is there any moves left to play
     if move_counter >= 9:
         start_over()
