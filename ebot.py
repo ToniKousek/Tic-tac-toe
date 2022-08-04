46414cef29e028c91ebbe6f334013722a801c0cb
DisguisedMamal <toni.kousek@gmail.com>
2022-08-04 17:48:23 +0200

    Made the easy bot functions

---
+++ ebot.py
@@ -0,0 +1,11 @@
+#all the functions made for the easy bot
+
+from random import randint
+
+
+def get_input(board, player_string):
+    flag = 1
+    while flag:
+        move = randint(1,9)
+        if board[move - 1] != player_string[0] and board[move - 1] != player_string[1]:
+            return move
